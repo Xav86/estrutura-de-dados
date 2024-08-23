5e8f7df742bb6a29c4852ba77d7150affe81ffad
Gustavo Goncalves Dos Santos <gustavo.330123@alunosatc.edu.br>
2024-08-23 19:33:54 -0300

    finalizado programa gerenciamento de livros

--- 16-08-24 - POO/gerenciamento-de-livros.py
+++ 16-08-24 - POO/gerenciamento-de-livros.py
@@ -1,43 +1,60 @@
 # Você precisa implementar três classes principais: Livro, Usuario, e Biblioteca. A classe Livro representa os livros da biblioteca, a classe Usuario representa as pessoas que pegam emprestado os livros, e a classe Biblioteca gerencia o processo de empréstimo de livros.
 
 class Livro:
-    def __init__(self, titulo, autor, disponivel):
+    def __init__(self, titulo, autor):
         self.titulo = titulo
         self.autor = autor
-        self.disponivel = disponivel
+        self.disponivel = True
     
     def exibirDetalhes(self):
         print(f"Titulo: {self.titulo}, Autor: {self.autor}, Disponivel: {self.disponivel}")
-        return
         
 class Usuario:
-    def __init__(self, nome, livrosEmprestados):
+    def __init__(self, nome):
         self.nome = nome
-        self.livrosEmprestados = livrosEmprestados
+        self.livrosEmprestados = []
     
-    def emprestarLivro(self):
-        print('a')
+    def emprestarLivro(self, livro):
+        if (livro.disponivel):
+            self.livrosEmprestados.append(livro)
+            livro.disponivel = False
+        else:
+            print(f'O livro {livro.titulo} não esta disponivel')
         
-    def devolverLivro(self):
-        print('a')
+    def devolverLivro(self, livro):
+        if (livro.disponivel == False):
+            self.livrosEmprestados.remove(livro)
+            livro.disponivel = True
         
 class Biblioteca:
     def __init__(self, nome):
         self.nome = nome
-        self.livro = []
+        self.livros = []
         
     def adicionarLivro(self, livro):
-        self.livro.append(livro)
+        self.livros.append(livro)
         
     def exibirLivrosDisponiveis(self):
-        for livro in self.livro:
-            livro.exibirDetalhes()
+        for livro in self.livros:
+            if (livro.disponivel):
+                livro.exibirDetalhes()
 
+
+
+l1 = Livro('Livro legal', 'Gustavo')
+l2 = Livro('Livro legal2', 'Gustavo2')
+l3 = Livro('Livro legal3', 'Gustavo3')
 
 biblioteca = Biblioteca("Biblioteca")
 
-biblioteca.adicionarLivro(Livro('Livro legal', 'Gustavo', True))
-biblioteca.adicionarLivro(Livro('Livro legal2', 'Gustavo2', False))
-biblioteca.adicionarLivro(Livro('Livro legal3', 'Gustavo3', True))
+biblioteca.exibirLivrosDisponiveis()
+
+biblioteca.adicionarLivro(l1)
+biblioteca.adicionarLivro(l2)
+biblioteca.adicionarLivro(l3)
+
+u1 = Usuario("coisa")
+u1.emprestarLivro(l1)
+u1.emprestarLivro(l1)
 
 biblioteca.exibirLivrosDisponiveis()
