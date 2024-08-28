4c1c19aef19648f35e47a791be7c28d5f8c9d708
Xav86 <gustavo.g.2005@gmail.com>
2024-08-28 11:13:02 -0300

    respondida a parte 2

--- 23-08-24 - Array-na-mao/vetor-nao-ordenado.py
+++ 23-08-24 - Array-na-mao/vetor-nao-ordenado.py
@@ -10,6 +10,7 @@ class VetorNaoOrdenado:
         else:
             self.ultimaPosicao += 1
             self.valores[self.ultimaPosicao] = valor
+            # print(self.ultimaPosicao)
 
     def imprimir(self):
         if (self.ultimaPosicao == -1):
@@ -34,26 +35,53 @@ class VetorNaoOrdenado:
             self.ultimaPosicao -= 1
 
 
+
+
+######## PARTE 1 
+# meuVetor = VetorNaoOrdenado(7)
+# meuVetor.imprimir()
+
+# meuNome = 'Gustavo'
+
+# for i in meuNome:
+#     print('--- inserindo a letara: ', i)
+#     meuVetor.inserir(i)
+#     meuVetor.imprimir()
+
+# print('Vetor atual: ')
+# meuVetor.imprimir()
+
+# print(meuVetor.pesquisar('g'))
+# print(meuVetor.pesquisar('G'))
+# print(meuVetor.pesquisar('u'))
+# print(meuVetor.pesquisar('v'))
+
+# meuVetor.excluir('G')
+# meuVetor.excluir('t')
+# meuVetor.excluir('o')
+
+# meuVetor.imprimir()
+
+######### PARTE 2
+
 meuVetor = VetorNaoOrdenado(7)
 meuVetor.imprimir()
 
-meuNome = 'Gustavo'
+sequencia = 'SATC'
 
-for i in meuNome:
-    print('--- inserindo a letara: ', i)
+for i in sequencia:
     meuVetor.inserir(i)
-    meuVetor.imprimir()
-
-print('Vetor atual: ')
+    
 meuVetor.imprimir()
 
-print(meuVetor.pesquisar('g'))
-print(meuVetor.pesquisar('G'))
-print(meuVetor.pesquisar('u'))
-print(meuVetor.pesquisar('v'))
-
-meuVetor.excluir('G')
-meuVetor.excluir('t')
-meuVetor.excluir('o')
-
+# a. ultimaPosicao
+# b. 4 posição
+# c. sera preciso 4, pesquisar pelo elemento seguinte e realocar uma posição para tras, e a mesma coisa para o seguinte, somando 4
+meuVetor.excluir('A')
+print('---')
+# d. Posição 0 | Valor S
+#    Posição 1 | Valor T
+#    Posição 2 | Valor C
 meuVetor.imprimir()
+#e. 3 posição
+#f. não retornar -1, ou melhor, retornar a posição do dado
