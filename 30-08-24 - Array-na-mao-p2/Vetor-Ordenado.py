33c7297e73fa025b6c373fb488b52ff66af87129
Xav86 <gustavo.g.2005@gmail.com>
2024-09-05 09:34:11 -0300

    feito lista de exercicios 04

--- 30-08-24 - Array-na-mao-p2/Vetor-Ordenado.py
+++ 30-08-24 - Array-na-mao-p2/Vetor-Ordenado.py
@@ -4,7 +4,8 @@ class VetorOrdenado:
     def __init__(self, capacidade):
         self.capacidade = capacidade
         self.ultimaPosicao = -1
-        self.valores = np.empty(self.capacidade, dtype=int)
+        # self.valores = np.empty(self.capacidade, dtype=int)
+        self.valores = [''] * capacidade
 
     def inserir(self, valor):
         if self.ultimaPosicao == self.capacidade -1:
@@ -33,6 +34,7 @@ class VetorOrdenado:
         
         self.valores[posicao] = valor
         self.ultimaPosicao += 1
+        # print('ultima posição',self.ultimaPosicao)
 
     def imprimir(self):
         if (self.ultimaPosicao == -1):
@@ -48,6 +50,7 @@ class VetorOrdenado:
         while True:
             posicaoAtual = int((limiteInf + limiteSup) / 2)
 
+            print('ultima posição',self.ultimaPosicao)
             # achou
             if (self.valores[posicaoAtual] == valor):
                 return posicaoAtual
@@ -89,22 +92,86 @@ class VetorOrdenado:
             self.ultimaPosicao -= 1
 
 
+# meuVetor = VetorOrdenado(7)
+# meuVetor.imprimir()
+
+# meuVetor.inserir(2)
+# meuVetor.inserir(666)
+# meuVetor.inserir(7)
+# meuVetor.inserir(25)
+# meuVetor.inserir(17)
+
+# meuVetor.imprimir()
+
+# print(meuVetor.pesquisar(17))
+# print(meuVetor.pesquisar(35)) #-1
+# print(meuVetor.pesquisar(25))
+
+# meuVetor.excluir(17)
+# meuVetor.imprimir()
+
+# meuVetor.excluir(55) #-1
+
+# meuVetor = VetorOrdenado(7)
+# meuVetor.imprimir()
+
+# meuNome = 'Gustavo'
+
+# for i in meuNome:
+#     print('--- inserindo a letara: ', i)
+#     meuVetor.inserir(i)
+
+# print('Vetor atual: ')
+# meuVetor.imprimir()
+
+# posicaoDaBusca = meuVetor.pesquisaBinaria('v')
+# print(posicaoDaBusca)
+# posicaoDaBusca = meuVetor.pesquisaBinaria('s')
+# print(posicaoDaBusca)
+# posicaoDaBusca = meuVetor.pesquisaBinaria('a')
+# print(posicaoDaBusca)
+
+# meuVetor.excluir('G')
+# meuVetor.excluir('t')
+# meuVetor.excluir('v')
+
+# meuVetor.imprimir()
+
+# Notei que meu nome tava estranho no inicio, mas é pq é feita ordenação né kk
+# outra coisa é que o esquema da biblioteca pra gerar um array sem propriedades e pá n sei como colocar pra ser string, tentei colocar literalmente string mas n vai, n sei se é
+# pra ser char ou algo assim mas resolvi voltar pra config antiga pra poder trabalhar.
+
+# Numero 2
+
+# meuVetor = VetorOrdenado(7)
+# meuVetor.imprimir()
+
+# numeros = [2, 3, 5, 7]
+
+# for i in numeros:
+#     meuVetor.inserir(i)
+#     print('---inserindo: ', i)
+    
+# meuVetor.imprimir()
+
+# meuVetor.inserir(4)
+
+# meuVetor.imprimir()
+
+# posicao = meuVetor.pesquisaBinaria(7);
+# print(posicao)
+
+#A) 3
+#B) 2
+#C) 4
+#D) 3
+#E) 3
+
+
+# Numero 3
+
 meuVetor = VetorOrdenado(7)
 meuVetor.imprimir()
 
-meuVetor.inserir(2)
-meuVetor.inserir(666)
-meuVetor.inserir(7)
-meuVetor.inserir(25)
-meuVetor.inserir(17)
+letras = ['A', 'C', 'D', 'F']
 
-meuVetor.imprimir()
-
-print(meuVetor.pesquisar(17))
-print(meuVetor.pesquisar(35)) #-1
-print(meuVetor.pesquisar(25))
-
-meuVetor.excluir(17)
-meuVetor.imprimir()
-
-meuVetor.excluir(55) #-1
