2483eadd0c3a5c0113ee3f6955a993dff7236017
Xav86 <gustavo.g.2005@gmail.com>
2024-09-11 11:05:17 -0300

    concluido exercico de pilha

--- 06-09-24 - Pilhas e Filas/Pilha.py
+++ 06-09-24 - Pilhas e Filas/Pilha.py
@@ -9,7 +9,8 @@ class Pilha:
             print('Pilha vazia')
         else:
             for i in range(self.topo + 1):
-                print(self.valores[i])
+                print(self.valores[i], end=" ")
+            print()
         
     def pilhaCheia(self):
         return self.topo == self.capacidade -1
@@ -36,39 +37,81 @@ class Pilha:
         else:
             return self.valores[self.topo]
         
-minhaPilha = Pilha(4)
+# minhaPilha = Pilha(4)
 
-resultado = minhaPilha.pilhaCheia()
-print(resultado)
+# resultado = minhaPilha.pilhaCheia()
+# print(resultado)
 
-print('1---')
+# print('1---')
 
-minhaPilha.empilhar(3)
-minhaPilha.empilhar(2)
-minhaPilha.empilhar(5)
-minhaPilha.empilhar(123)
+# minhaPilha.empilhar(3)
+# minhaPilha.empilhar(2)
+# minhaPilha.empilhar(5)
+# minhaPilha.empilhar(123)
 
-print('2---')
+# print('2---')
 
-minhaPilha.imprimir()
+# minhaPilha.imprimir()
 
-print('3---')
+# print('3---')
 
-minhaPilha.empilhar(666) # não vai entra
+# minhaPilha.empilhar(666) # não vai entra
 
-print('4---')
+# print('4---')
 
-minhaPilha.imprimir()
+# minhaPilha.imprimir()
 
-print('5---')
+# print('5---')
 
-topo = minhaPilha.verTopo()
-print(topo)
+# topo = minhaPilha.verTopo()
+# print(topo)
 
-print('6---')
+# print('6---')
 
+# minhaPilha.desempilhar()
+
+# print('7---')
+
+# minhaPilha.imprimir()
+
+# LISTA DE ATIVIDADES PARTE PILHA
+
+minhaPilha = Pilha(7)
+
+# NUMERO 7
+# A
 minhaPilha.desempilhar()
 
-print('7---')
+# B
+meuNome = 'Gustavo'
+for i in meuNome:
+    minhaPilha.empilhar(i)
+    minhaPilha.imprimir()
 
-minhaPilha.imprimir()
+# C
+minhaPilha.empilhar('coisa')
+
+# D
+topo = minhaPilha.verTopo()
+print('Topo da fila: ', topo)
+
+# E
+for i in range(3):
+    minhaPilha.desempilhar()
+    
+print('---')
+topo = minhaPilha.verTopo()
+print('Topo da fila: ', topo)
+
+
+# NUMERO 8
+# "S"
+
+# NUMERO 9
+# ficar vendo a posição do topo para determinar as ações (sinceramente n sei qual é o problema, falei algo q acho meio chatinho mesmo)
+
+# NUMERO 10
+# Se o topo for igual a -1
+
+# NUMERO 11
+# se a posição do topo for igual ao tamanho da pila (capacidade -1)
